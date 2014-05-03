5ffee42b4c142e13600a75e3c0609de9e41bb04b
Proyecto Maternidad <maternidadproyecto@gmail.com>
2014-05-03 10:31:58 -0430

    DEl-FIX Eliminar carpeta config

--- config/cerrar.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-require_once './Salir.php';
-
-$cerrar = new Salir();
-$cerrar->exitSesion();
--- config/valida_sesion.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-if(isset( $_SESSION['id_session']) && isset($_SESSION['date_session'])){
-    header('location:menu.php');
-}
